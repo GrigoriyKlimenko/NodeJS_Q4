2797c3e311091e02e60190af14fc132aebadf03b
grigoriy_klimenko <gklimenko>
2021-11-14 11:17:28 +0300

    add crypt tool

--- src/consoleArguments.js
+++ src/consoleArguments.js
@@ -5,20 +5,20 @@ const optionsString = process.argv.slice(2);
 const optionsToCheck = ['-c', '-i', '-o']
 
 const buildConfig = (optionsString) => {
-    let optionsObject = {};
+  let optionsObject = {};
 
-    if (isValidOptionsString(optionsString)) {
-        optionsToCheck.map((option) => {
-            if (optionsString.includes(option)) {
-                optionsObject[option] = optionsString[optionsString.indexOf(option) + 1]
-            }
-        })
-    }
-    optionsObject['-c'] = optionsObject['-c'].split('-');
+  if (isValidOptionsString(optionsString)) {
+    optionsToCheck.map((option) => {
+      if (optionsString.includes(option)) {
+        optionsObject[option] = optionsString[optionsString.indexOf(option) + 1].trim();
+      }
+    })
+  }
+  optionsObject['-c'] = optionsObject['-c'].split('-').map((item) => item.trim());
 
-    if (isValidOptions(optionsObject)) {
-        return optionsObject;
-    }
+  if (isValidOptions(optionsObject)) {
+    return optionsObject;
+  }
 }
 
 
@@ -26,5 +26,5 @@ const buildConfig = (optionsString) => {
 const getConfig = () => buildConfig(optionsString);
 
 module.exports = {
-    getConfig,
+  getConfig,
 };
--- src/validation.js
+++ src/validation.js
@@ -2,65 +2,65 @@ const { accessSync, constants } = require('fs');
 const { UpperCaseError, CountOptionsError, FileAvailableError, ConfigValidationError } = require('./userErrors');
 
 let countOfOptions = {
-    '-i': 0,
-    '-o': 0,
-    '-c': 0,
+  '-i': 0,
+  '-o': 0,
+  '-c': 0,
 }
 
 const isValidOptionsString = (configString) => {
-    configString.forEach(element => {
-        if (countOfOptions.hasOwnProperty(element)) {
-            countOfOptions[element] += 1;
-        }
-        if (element.toUpperCase() === element && countOfOptions.hasOwnProperty(element.toLowerCase())) {
-            throw new UpperCaseError(element);
-        }
-    });
-    if (countOfOptions['-c'] === 0) {
-        throw new CountOptionsError();
+  configString.forEach(element => {
+    if (countOfOptions.hasOwnProperty(element)) {
+      countOfOptions[element] += 1;
     }
-    for (let key in countOfOptions) {
-        if (countOfOptions.hasOwnProperty(key) && countOfOptions[key] > 1) {
-            throw new CountOptionsError(key);
-        }
+    if (element.toUpperCase() === element && countOfOptions.hasOwnProperty(element.toLowerCase())) {
+      throw new UpperCaseError(element);
     }
-    return true;
+  });
+  if (countOfOptions['-c'] === 0) {
+    throw new CountOptionsError();
+  }
+  for (let key in countOfOptions) {
+    if (countOfOptions.hasOwnProperty(key) && countOfOptions[key] > 1) {
+      throw new CountOptionsError(key);
+    }
+  }
+  return true;
 }
 
 const isAvailableFile = (path, constant) => {
-    try {
-        let y = accessSync(path, constant);
-        return true;
-    }
-    catch {
-        return false;
-    }
+  try {
+    let y = accessSync(path, constant);
+    return true;
+  }
+  catch {
+    return false;
+  }
 }
 
 const isValidConfig = (config) => {
-    let isValid = true;
-    config.forEach((item) => {
-        if (!item.trim().match(/^[CR][0-1]$/) && !item.trim().match(/^A$/)) {
-            isValid = false;
-        }
-    })
-    return isValid;
+  let isValid = true;
+  config.forEach((item) => {
+    if (!item.trim().match(/^[CR][0-1]$/) && !item.trim().match(/^A$/)) {
+      isValid = false;
+    }
+  })
+  return isValid;
 }
 
 const isValidOptions = (options) => {
-    if (options.hasOwnProperty('-i') && !isAvailableFile(options['-i'], constants.R_OK)) {
-        throw new FileAvailableError('Input');
-    }
-    if (options.hasOwnProperty('-o') && !isAvailableFile(options['-o'], constants.W_OK)) {
-        throw new FileAvailableError('Output')
-    }
-    if (options.hasOwnProperty('-c') && !isValidConfig(options['-c'])) {
-        throw new ConfigValidationError()
-    }
-    return options;
+  if (options.hasOwnProperty('-i') && !isAvailableFile(options['-i'], constants.R_OK)) {
+    throw new FileAvailableError('Input');
+  }
+  if (options.hasOwnProperty('-o') && !isAvailableFile(options['-o'], constants.W_OK)) {
+    throw new FileAvailableError('Output')
+  }
+  if (options.hasOwnProperty('-c') && !isValidConfig(options['-c'])) {
+    throw new ConfigValidationError()
+  }
+  return options;
 }
 
 module.exports = {
-    isValidOptionsString,
-    isValidOptions,
+  isValidOptionsString,
+  isValidOptions,
 }
